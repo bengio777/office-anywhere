efdd749254c3707316b7908eeeacf221f67d9d75
Thomas N Rizzo III <thomasnrizzoiii@Thomass-MacBook-Pro.local>
2016-09-26 16:29:46 -0600

    Location page formatting, comments are rendering from seed files.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var queries = require('../db/queries')
 
 router.get('/', getHomePage); //Retrieves '/'
 router.get('/locations', getLocationPage); // Retrieves selected location
@@ -12,7 +12,14 @@ function getHomePage(req, res, next) {
 }
 
 function getLocationPage(req, res, next) {
-    res.render('locations')
+    queries.Comments().orderBy('id', 'asc')
+        .then(function(data) {
+            res.render('locations', {
+                title: 'Office Anywhere',
+                comments: data
+            })
+        })
 }
 
 module.exports = router;
+rts = router;
