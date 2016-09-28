ad902b20bb670a2d5e2f9ee2ca0adc76588748a1
Thomas N Rizzo III <thomasnrizzoiii@Thomass-MacBook-Pro.local>
2016-09-28 14:10:58 -0600

    Pre merge commit w BG

--- routes/locations.js
+++ routes/locations.js
@@ -1,3 +1,4 @@
+var admin;
 var express = require('express');
 var router = express.Router();
 var queries = require('../db/queries')
@@ -7,31 +8,38 @@ var queries = require('../db/queries')
 router.get('/', getLocationsPage); // Retrieves selected location
 
 function getLocationsPage(req, res, next) {
-    queries.Comments().orderBy('id', 'asc')
+  if(req.isAuthenticated()){
+    var tt = req.user.role
+    if (
+     tt != 'admin'|| tt==undefined){
+      admin = false;
+    }else{
+      admin = true;
+    }
+  }
 
+    queries.Comments().orderBy('id', 'asc')
         .then(function(data) {
             res.render('locations', {
                 title: 'Office Anywhere',
                 brand: 'Office Anywhere',
-                verify:req.isAuthenticated(),
-                comments: data
+                verify: req.isAuthenticated(),
+                comments: data,
+                admin : admin
             })
         })
 }
 
 
 router.post('/locations', function(req, res, next) {
-  console.log('this is the TITLE: ' + req.body.title);
             queries.addComments(req.body.title,req.body.body)
                 .then(function() {
                     queries.Comments()
                     .then(function(comments){
-                      console.log(comments);
                       res.render('locations',{comments:comments, verify:req.isAuthenticated()})
-
                     })
                 })
             })
 
 
-        module.exports = router;
+module.exports = router;
