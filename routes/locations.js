cf4ba27762f124de852319198d4f7418cab54308
Thomas N Rizzo III <thomasnrizzoiii@Thomass-MacBook-Pro.local>
2016-09-28 15:58:32 -0600

    pre merge commit w TH

--- routes/locations.js
+++ routes/locations.js
@@ -3,48 +3,43 @@ var express = require('express');
 var router = express.Router();
 var queries = require('../db/queries')
 
-
-
-router.get('/', getLocationsPage); // Retrieves selected location
-router.post('/', postComment); // Posts a comment 
-
+router.get('/:id', getLocationsPage); // Retrieves selected location
+router.post('/', postComment); // Posts a comment
 
 function getLocationsPage(req, res, next) {
   queries.Comments().orderBy('id', 'asc')
-        .then(function(data) {
-          for(var i in data){
+    .then(function(data) {
+      for(var i in data){
         if(req.isAuthenticated()){
           var userRole = req.user.role
-          if (
-           userRole != 'admin'|| userRole==undefined){
-            data[i].admin = false;
-          }else{
-            data[i].admin = true;
-          }
-        }
-        else{
+        if (
+          userRole != 'admin'|| userRole==undefined){
           data[i].admin = false;
-        }
+        }else{
+          data[i].admin = true;
+          }
+        }else{
+          data[i].admin = false;
+          }
       }
-            res.render('locations', {
-                title: 'Office Anywhere',
-                brand: 'Office Anywhere',
-                verify: req.isAuthenticated(),
-                comments: data,
-                admin : data.admin
-            })
-        })
-    }
-
+          res.render('locations', {
+            title: 'Office Anywhere',
+            brand: 'Office Anywhere',
+            verify: req.isAuthenticated(),
+            comments: data,
+            admin : data.admin
+    })
+  })
+}
 
 function postComment(req, res, next) {
-            queries.addComments(req.body.title,req.body.body)
-                .then(function() {
-                    queries.Comments()
-                    .then(function(comments){
-                      res.render('locations',{comments:comments, verify:req.isAuthenticated()})
-                    })
-                })
-            }
+  queries.addComments(req.body.title,req.body.body)
+    .then(function() {
+      queries.Comments()
+        .then(function(comments){
+          res.render('locations',{comments:comments, verify:req.isAuthenticated()})
+    })
+  })
+}
 
 module.exports = router;
