6e8780dc32af3e2f63df56a647613402b1faec76
Thomas N Rizzo III <thomasnrizzo@gmail.com>
2016-09-29 10:30:24 -0600

    Dynamic routes generating correctly. Ready for push.

--- routes/locations.js
+++ routes/locations.js
@@ -11,32 +11,31 @@ router.post('/', postComment); // Posts a comment
 
 function getLocationsPage(req, res, next) {
   axios.get(`http://api.workfrom.co/places/${req.params.id}?appid=${process.env.WORKFROM_API_KEY}`)
-  .then(function(result){
-  // console.log(result.data.response);
-  venue = result.data.response;
-})
-  queries.Comments().orderBy('id', 'asc')
-    .then(function(data) {
-      for(var i in data){
-        if(req.isAuthenticated()){
-          var userRole = req.user.role
-        if (
-          userRole != 'admin'|| userRole==undefined){
-          data[i].admin = false;
-        }else{
-          data[i].admin = true;
-          }
-        }else{
-          data[i].admin = false;
+    .then(function(result){
+    venue = result.data.response;
+    queries.Comments().orderBy('id', 'asc')
+        .then(function(data) {
+        for(var i in data){
+            if(req.isAuthenticated()){
+              var userRole = req.user.role
+            if (
+              userRole != 'admin'|| userRole==undefined){
+              data[i].admin = false;
+            }else{
+              data[i].admin = true;
+              } //DO NOT DELETE THIS CURLY. THE CURLIES LOOK WEIRD HERE, BUT THEY ARE CORRECT
+            }else{
+                data[i].admin = false;
           }
       }
-          res.render('locations', {
-            venue: venue,
-            title: 'Office Anywhere',
-            brand: 'Office Anywhere',
-            verify: req.isAuthenticated(),
-            comments: data,
-            admin : data.admin
+            res.render('locations', {
+              venue: venue,
+              title: 'Office Anywhere',
+              brand: 'Office Anywhere',
+              verify: req.isAuthenticated(),
+              comments: data,
+              admin : data.admin
+      })
     })
   })
 }
